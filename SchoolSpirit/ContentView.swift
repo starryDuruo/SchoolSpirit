c43e99e79e9de6a171bdf6f971b37132aea0bde8
Sige Wang <wangfiz@bc.edu>
2026-02-02 18:08:39 -0500

    done

--- SchoolSpirit/ContentView.swift
+++ SchoolSpirit/ContentView.swift
@@ -8,12 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.thin)
+            
+            Spacer()
+            
+            HStack {
+                Button("Who Are We?", systemImage: "graduationcap") {
+                    message = "We Are BC!"
+                    imageName = "eagleLogo"
+                    
+                }
+                // use Button(action:label:)
+                //            label:{
+                //                Label("", systemImage:"")
+                //              //Text()
+                //              //Image()
+                //            }
+                Spacer()
+                Button ("🦅 How is Baldwin?"){
+                    message = "Flying High!"
+                    imageName = "baldwin"
+                }
+            }
+            .buttonStyle(.glassProminent)
+            .bold()
+            .tint(.goldBC)
+            .foregroundStyle(.maroonBC)
+            
+            
         }
         .padding()
     }
