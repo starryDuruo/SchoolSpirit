4587d222b0faf71c024d1d6abf52a6134152f4a8
Sige Wang <wangfiz@bc.edu>
2026-02-02 16:51:01 -0500

    Initial Commit

---
+++ SchoolSpirit/SchoolSpiritApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolSpiritApp.swift
+//  SchoolSpirit
+//
+//  Created by Wang Sige on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct SchoolSpiritApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
